6014c7d9908adb072711ac251e622d589239d8d5
Marson <marsoncarlbarreda@gmail.com>
2025-06-17 10:31:20 +0800

    fix the files for html, css, js, and images

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    window.location.href = 'index.html';
+    window.location.href = '../html/index.html';
 });
